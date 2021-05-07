32c14daf4bd166a23205509a85920452103fb791
PatelPaavan <patelpaavan10@gmail.com>
2021-05-06 21:06:24 -0400

    Challenge # 1

---
+++ challenge _1.js
@@ -0,0 +1,24 @@
+
+console.log("\nWelcome to the Coding Challenge");
+
+//Test Data 1
+let markMass = 78;
+let markHeight = 1.69;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+console.log("Test Data 1")
+console.log("BMI of Mark: ",markMass/markHeight**2)
+console.log("BMI of John: ",johnMass/johnHeight**2)
+
+//Test Data 2
+
+markMass = 95;
+markHeight = 1.88;
+
+johnMass = 85;
+johnHeight = 1.76;
+console.log("Test Data 2")
+console.log("BMI of Mark: ",markMass/markHeight**2)
+console.log("BMI of John: ",johnMass/johnHeight**2)
